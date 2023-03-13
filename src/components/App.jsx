a982b23ee43e7f3aad2b3da47806120bb538293b
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-03-13 22:09:29 +0200

    Add axios request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from 'react';
+import { requestCourses } from 'services/api';
+import { Loader } from './Loader/Loader';
+
 export const App = () => {
+  const [courses, setCourses] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        setIsLoading(true);
+        const fetchedCourses = await requestCourses();
+        setCourses(fetchedCourses);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {isLoading && <Loader />}
+      {error !== null && <p>Oops, some error occured... {error}</p>}
     </div>
   );
 };
---
+++ src/services/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzNjcwOTlkYy1kNjE2LTQzNjEtYjNhYy00MzUzMmE2NjQwNTciLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg3MzYzMTEsImV4cCI6MTY3OTYzNjMxMX0.yLpxbfFvaDoUYrm_iLiaKI0oLFONGtyIR65pWg_kOy4';
+
+// const BASE_URL = 'http://api.wisey.app/api/v1/core/preview-courses';
+
+// const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '33371810-0a52094386dea8583f801697d';
+// export const PER_PAGE = 12;
+
+export const requestCourses = async () => {
+  // const searchParams = {
+  //   params: {
+  //     key: API_KEY,
+  //     q: query,
+  //     image_type: 'photo',
+  //     orientation: 'horizontal',
+  //     page: page,
+  //     per_page: PER_PAGE,
+  //   },
+  // };
+
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  const { data } = await axios.get(
+    'http://api.wisey.app/api/v1/core/preview-courses',
+    config
+  );
+
+  return data;
+};
